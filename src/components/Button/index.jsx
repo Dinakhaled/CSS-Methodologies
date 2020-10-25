8cae5e92db5d6a29c3cc068c1307c75fead4cc80
DinaKhaled <dina.elghndour@gmail.com>
2020-10-25 18:20:10 +0200

    Try React-jss

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { createUseStyles } from "react-jss";
 
-const Button = () => {
-  return <button>Click here</button>;
+const useStyles = createUseStyles({
+  button: {
+    color: "green",
+    padding: {
+      top: 10,
+      right: 15,
+      bottom: 10,
+      left: 15,
+    },
+    "&.primary": {
+      backgroundColor: "#000",
+      color: "#fff",
+      fontSize: (props) => props.fontSize,
+    },
+  },
+});
+
+const Button = ({ className = "", ...props }) => {
+  const classes = useStyles(props);
+  return (
+    <button className={`${classes.button} ${className}`}>Click here</button>
+  );
+};
+
+Button.defaultProps = {
+  fontSize: "80px",
 };
 
 export default Button;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Button from "../../components/Button";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <h1>Home page</h1>
+      <Button className="primary" fontSize="40px" />
+    </div>
+  );
 };
 
 export default Home;
