58a9a210571fbec8fe443bc0fa955a8ac9779f5f
DinaKhaled <dina.elghndour@gmail.com>
2020-10-25 15:17:31 +0200

    Try css module

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./button.module.scss";
 
-const Button = () => {
-  return <button>Click here</button>;
+const Button = ({ className = "" }) => {
+  return (
+    <button className={`${styles.button} ${styles[`${className}`]}`}>
+      Click here
+    </button>
+  );
 };
 
 export default Button;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Button from "../../components/Button";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <h1>Home page</h1>
+      <Button className="primary" />
+      <button className="button primary">hello</button>
+    </div>
+  );
 };
 
 export default Home;
